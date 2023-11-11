5bc15a6c4d81374b4caef15c9b1eb1ffdb9902ae
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-11-11 13:25:05 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1260,13 +1260,6 @@ class PlayState extends MusicBeatState
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
 		add(botplayTxt);
-				
-			var creditTxt = new FlxText(876, 648, 348);
-     creditTxt.text = "PORTED BY\nFNF BR";
-    creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
-    creditTxt.scrollFactor.set();
-    add(creditTxt);
-				
 		if(ClientPrefs.downScroll) {
 			botplayTxt.y = timeBarBG.y - 78;
 		}
@@ -1277,8 +1270,7 @@ class PlayState extends MusicBeatState
 		blackvg.cameras = [camOther];
 		add(blackvg);
 
-		strumLineNotes.cameras = [camHUD];
-		creditTxt.cameras = [camHUD];		
+		strumLineNotes.cameras = [camHUD];		
 		grpNoteSplashes.cameras = [camHUD];
 		notes.cameras = [camHUD];
 		healthBar.cameras = [camHUD];
